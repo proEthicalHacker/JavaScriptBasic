96b113fa1216e369fadf2596b8770ec09796635b
MR Denny react-native developer <132824181+proEthicalHacker@users.noreply.github.com>
2023-11-06 19:36:33 +0000

    Conversion of data type

---
+++ Basic/02_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = null //["esme value hogi to wo string hoti hai "] [null ki value 0 ayegi ye temprecher main use ho]
+
+// console.log(typeof(score))
+
+let valueInNuber= Number(score)
+// console.log(typeof valueInNuber);
+// console.log(valueInNuber);// NaN spacile type hai convert to hoga but value ye show hoti hai 
+
+//"33"=> 33
+//"33abc"=> NaN
+//true=> 1
+//"name" jo string hai num nhi to wo convert nhi hota hai show Nan karega
+
+
+let isLogIn ="denny"
+
+let booleanIsLogIn=Boolean(isLogIn)
+
+// console.log(booleanIsLogIn);
+
+//1 => true;0 =>false
+//"" => false "de" => true
+
+let someNumber="33abc"
+
+let stringNumber=String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
