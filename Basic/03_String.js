33c44f343091754c52e40f55a31adeac26f7e888
MR Denny react-native developer <132824181+proEthicalHacker@users.noreply.github.com>
2023-11-08 18:07:59 +0000

    String

---
+++ Basic/03_String.js
@@ -0,0 +1,29 @@
+//Topics [split,trim,replace , charAt,indexof,include,blink,bold,charecter,fixed,concat,contractor,big,]
+
+const name="dennnny"
+const repoCount=50
+
+console.log(`hello my name is ${name} and my repo is ${repoCount}`);
+
+let gameName ="memokkkhggfgjhkfjfjcjcjcxkyilbhkojkj"
+let gameCount=100
+
+
+const  newString= gameName.substring(0,4)
+// console.log(gameName);
+
+const anotherString = gameName.slice(-8,4)
+// console.log(anotherString);
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf("o"));
+// console.log(gameName.lastIndexOf("g"));
+
+const newStringOne="        Denny           " //trim se memory space ko kam,krta hai
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+
+const url= "https//denny.com/denny%20cool" //replase method
+console.log(url.replace('%20','-'));
+
+console.log(url.includes('doky')); //word ko find bhi kar skte hai      
