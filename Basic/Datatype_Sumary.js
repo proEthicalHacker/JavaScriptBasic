0f3403eabcbf9f4742603bc7265f65245cc46e16
MR Denny react-native developer <132824181+proEthicalHacker@users.noreply.github.com>
2023-11-08 15:59:48 +0530

    Datatype_Sumary

---
+++ Basic/Datatype_Sumary.js
@@ -0,0 +1,23 @@
+//Stack(Primitive) ,Heap(non-Primitive)[jab esse obj ref lene par oregnal value milti hai na ki copy]
+//Stack[Num,Boolan,String etc esme jab bi ref lenge toh copy milega]
+
+let myYoutubeName="DennyCool"
+
+let anotherName=myYoutubeName
+
+anotherName="Coffee aur Code"
+
+// console.log(myYoutubeName);
+
+
+let userOne ={
+    email:"user@gmail.com",
+    upi:"user@ybl"
+}
+
+let userTwo=userOne
+
+userTwo.email="namo@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
