de9965966661b3e3214400f0f0e65376e8e8d331
MR Denny react-native developer <132824181+proEthicalHacker@users.noreply.github.com>
2023-11-10 08:28:01 +0000

    04_Comparision

--- Basic/02_conversionOperation.js
+++ Basic/02_conversionOperation.js
@@ -2,7 +2,7 @@ let score = null //["esme value hogi to wo string hoti hai "] [null ki value 0 a
 
 // console.log(typeof(score))
 
-let valueInNuber= Number(score)
+let valueInNuber = Number(score)
 // console.log(typeof valueInNuber);
 // console.log(valueInNuber);// NaN spacile type hai convert to hoga but value ye show hoti hai 
 
@@ -12,18 +12,55 @@ let valueInNuber= Number(score)
 //"name" jo string hai num nhi to wo convert nhi hota hai show Nan karega
 
 
-let isLogIn ="denny"
+let isLogIn = "denny"
 
-let booleanIsLogIn=Boolean(isLogIn)
+let booleanIsLogIn = Boolean(isLogIn)
 
 // console.log(booleanIsLogIn);
 
 //1 => true;0 =>false
 //"" => false "de" => true
 
-let someNumber="33abc"
+let someNumber = "33abc"
 
-let stringNumber=String(someNumber)
+let stringNumber = String(someNumber)
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+/////******************************Oprations********************************************* */
+
+let value = 3
+let negValue = -value
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+let str1 = "hello"
+let str2 = "Denny"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("2"+4);
+// console.log(6+"2");
+// console.log((1+2)+"3");
+
+console.log(+true);
+
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 4
+
+let gameCounter = 100
+gameCounter++;
+
+console.log(gameCounter);
