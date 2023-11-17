f679dca9ec61da9a8ae0c9fddf7d502546cbabe2
MR Denny react-native developer <132824181+proEthicalHacker@users.noreply.github.com>
2023-11-17 09:26:26 +0000

    DateTime.js

---
+++ Basic/07_DateTime.js
@@ -0,0 +1,33 @@
+//Date 
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+
+// let myCreatedDate= new Date(2023,1,23)
+// let myCreatedDate= new Date(2023,1,23,5,3)
+// let myCreatedDate= new Date("2023-1-11")
+
+ let myCreatedDate= new Date("01-14-2023")
+
+let myTimeStamp= Date.now()
+
+// // console.table(myCreatedDate.toDateString());
+
+// // console.log(myTimeStamp);
+// // console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+
+let newDate=new Date()
+
+// console.log(newDate);
+// console.log(newDate.getDate());
+// console.log(newDate.getDay());
+
+
+// `${newDate.getDay()} and the time ${newDate.getTime}`
+
